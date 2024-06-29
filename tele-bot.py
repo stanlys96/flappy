5f9211d23a6c9d45e27110e6738a5ac70855e264
stanlys96 <stanlyskwok@gmail.com>
2024-06-29 21:53:52 +0700

    wlao

--- tele-bot.py
+++ tele-bot.py
@@ -1,29 +1,24 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, InlineQueryHandler, CallbackContext, InlineQueryResultArticle, InputTextMessageContent
 
-# Define a start command handler
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Welcome to my game! Type /play to start.')
+def inline_query(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    if query == "play_game":
+        game_url = "hhttps://flappy-ochre.vercel.app/"
+        results = [
+            InlineQueryResultArticle(
+                id="1",
+                title="Play Game",
+                input_message_content=InputTextMessageContent(game_url)
+            )
+        ]
+        update.inline_query.answer(results)
 
-# Define a play command handler
-def play(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Game started! Here are the rules...')
-
-def main() -> None:
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("7075835530:AAHlNztDkDNWDbgJTkvgAQ8diUEdUa69Yho")
-
-    # Get the dispatcher to register handlers
+def main():
+    updater = Updater(token='7075835530:AAHlNztDkDNWDbgJTkvgAQ8diUEdUa69Yho', use_context=True)
     dispatcher = updater.dispatcher
-
-    # Register command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("play", play))
-
-    # Start the Bot
+    dispatcher.add_handler(InlineQueryHandler(inline_query))
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT.
     updater.idle()
 
 if __name__ == '__main__':
